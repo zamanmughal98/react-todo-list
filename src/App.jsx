e97787df1fe8966fa57abe6998e5e48df3b129f0
Zaman Bin Ishfaq <zaman.bin@greybeardsupport.com>
2025-03-17 16:37:04 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './styles/App.css'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/components/data.js
@@ -0,0 +1,6 @@
+export const ToDoListData = [
+    { id: 1, title: 'take out the trash', completed: false },
+    { id: 2, title: 'Task 2', completed: true },
+    { id: 3, title: 'Task 3', completed: false },
+  
+]
