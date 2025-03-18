7335a97c6f7b91d9d8dab262272fd86307047056
Zaman Mughal <zamanmughal98@gmail.com>
2025-03-18 23:06:52 +0500

    .prettierrc settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import { useState } from 'react';
 import { ToDoListData } from './components/data.js';
 import CreateNewTask from './components/CreateNewTask';
 const App = () => {
-    const [newTask, setNewTask] = useState('');
-    const handleChange = (e) => setNewTask(e.target.value);
+  const [newTask, setNewTask] = useState('');
+  const handleChange = (e) => setNewTask(e.target.value);
 
-    return (
-        <>
-            <CreateNewTask value={newTask} onChangeHandler={handleChange} />
+  return (
+    <>
+      <CreateNewTask value={newTask} onChangeHandler={handleChange} />
 
-            <section className="viewTasksContainer">
-                {ToDoListData.map(({ id, title }) => (
-                    <li className="viewTaskItem" key={id}>
-                        <input type="checkbox" />
-                        {id} {title.toLocaleLowerCase()}
-                    </li>
-                ))}
-            </section>
-        </>
-    );
+      <section className="viewTasksContainer">
+        {ToDoListData.map(({ id, title }) => (
+          <li className="viewTaskItem" key={id}>
+            <input type="checkbox" />
+            {id} {title.toLocaleLowerCase()}
+          </li>
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default App;
