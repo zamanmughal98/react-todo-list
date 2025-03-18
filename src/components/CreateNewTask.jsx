3ce141f5fe3404eb7f358d1d399f7b96b86f23db
Zaman Mughal <zamanmughal98@gmail.com>
2025-03-18 21:39:22 +0500

    .prettierrc settings

--- src/components/CreateNewTask.jsx
+++ src/components/CreateNewTask.jsx
@@ -1,31 +1,28 @@
 import { FaPlus } from 'react-icons/fa';
 const CreateNewTask = ({ value, onChangeHandler }) => {
-    return (
-        <>
-            <h1 className="title">React To-Do List</h1>
+  return (
+    <>
+      <h1 className="title">React To-Do List</h1>
 
-            <section className="createTaskContainer">
-                <input
-                    className="inputField"
-                    placeholder="Enter a new task..."
-                    type="text"
-                    name="inputTask"
-                    value={value}
-                    onChange={onChangeHandler}
-                    aria-label="New Task Input"
-                />
+      <section className="createTaskContainer">
+        <input
+          className="inputField"
+          placeholder="Enter a new task..."
+          type="text"
+          name="inputTask"
+          value={value}
+          onChange={onChangeHandler}
+          aria-label="New Task Input"
+        />
 
-                <button
-                    className="addButton"
-                    type="button"
-                    aria-label="Add Task">
-                    <span className="iconsWrapper">
-                        Add <FaPlus />
-                    </span>
-                </button>
-            </section>
-        </>
-    );
+        <button className="addButton" type="button" aria-label="Add Task">
+          <span className="iconsWrapper">
+            Add <FaPlus />
+          </span>
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default CreateNewTask;
