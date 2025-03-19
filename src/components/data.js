6d4c866d6e0ac71dbc11137e64f98ad0fdf3138c
Zaman Mughal <zamanmughal98@gmail.com>
2025-03-19 17:55:10 +0500

    .prettierrc settings

--- src/components/data.js
+++ src/components/data.js
@@ -1,9 +1,9 @@
 export const ToDoListData = [
-    {id: 1,title: 'take out the trash', isCompleted: true},
-    {id: 2,title: 'buy groceries', isCompleted: false},
-    {id: 3,title: 'finish coding project', isCompleted: true},
-    {id: 4,title: 'water the plants', isCompleted: false},
-    {id: 5,title: 'call mom', isCompleted: false},
-    {id: 6,title: 'clean the room', isCompleted: false},
-    {id: 7,title: 'go for a walk', isCompleted: false},
-  ];
+  { id: 1, title: 'take out the trash', isCompleted: true },
+  { id: 2, title: 'buy groceries', isCompleted: false },
+  { id: 3, title: 'finish coding project', isCompleted: true },
+  { id: 4, title: 'water the plants', isCompleted: false },
+  { id: 5, title: 'call mom', isCompleted: false },
+  { id: 6, title: 'clean the room', isCompleted: false },
+  { id: 7, title: 'go for a walk', isCompleted: false },
+];
